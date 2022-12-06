98757f1c953808afb3f5e1423d4d7a98950b01bd
Alexandregv1080p <alexandregv1999@gmail.com>
2022-12-06 14:09:21 -0300

    começando Node

---
+++ Node/app.js
@@ -0,0 +1,8 @@
+//const mod1 = require('./mod1')
+const {nome, sobrenome, falaNome} = require('./mod1')
+const { Pessoa } = require('./mod1-2')
+console.log(nome)
+
+const p1 = new Pessoa("Alexandre")
+
+console.log(p1)
---
+++ Node/mod1.js
@@ -0,0 +1,9 @@
+const nome = 'Luiz'
+const sobrenome = 'Mirando'
+
+const nomeCompleto = () =>{ nome,sobrenome }
+
+exports.nome = nome
+exports.sobrenome = sobrenome
+exports.nomeCompleto = nomeCompleto
+//console.log(module.exports)
